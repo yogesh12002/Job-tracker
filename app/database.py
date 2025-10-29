fe0b963a8db23b411f94c44a037b1f724d9121ce
Yogesh Gupta <yogeshgupta12002@gmail.com>
2025-10-29 21:47:24 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -6,6 +6,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./applications.db")
 
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
