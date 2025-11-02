0d678cec97457906b16cc28c21885b88402dd14d
Yogesh Gupta <yogeshgupta12002@gmail.com>
2025-11-02 21:44:19 +0530

    Add credential loading in get_gmail_service function

--- app/gmail_service.py
+++ app/gmail_service.py
@@ -7,6 +7,7 @@ from googleapiclient.discovery import build
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
 def get_gmail_service():
+    
     creds = None
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
