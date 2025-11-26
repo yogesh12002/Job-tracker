fd33b182f55b3fda2c42c3ef0cf07e0cc9b3f164
Yogesh Gupta <yogeshgupta12002@gmail.com>
2025-11-26 18:54:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ def get_db():
     return db
     
 
+
 # ------------------- Handlers ------------------- #
 async def start(update: Update, context: CallbackContext):
     await update.message.reply_text(
