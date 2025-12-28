e493fc613ad333be4fdff29aed735ecb91f44a21
Yogesh Gupta <yogeshgupta12002@gmail.com>
2025-12-28 21:41:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from app import database, models
 from app.main import sync_emails_job   # reuse Gmail sync
 
 
+
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")  # set in your .env
 
 # ------------------- DB Dependency ------------------- #
